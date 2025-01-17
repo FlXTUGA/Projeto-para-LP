a96614523fcd867626e9fb0a009506ba795d8b17
FlXTUGA <fixtuga@gmail.com>
2025-01-17 22:28:50 +0000

    Update cliente.h

--- Projeto para LP/cliente.h
+++ Projeto para LP/cliente.h
@@ -1,31 +1,32 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
-#include <stdlib.h>
+#include<stdlib.h>
 #include <string.h>
+
 
 #define MAX_CLIENTES 100
 
 typedef enum {
-    REGULAR,
-    ESPORADICO,
-    OCASIONAL,
+	REGULAR,
+	ESPORADICO,
+	OCASIONAL,
+
 } TipoCliente;
 
 typedef struct {
-    int numerocliente; 
-    char nomecliente[50]; 
-    char nif[10]; 
-    float valor; 
-    TipoCliente tipo; 
-    char dataregisto[11]; 
-} TCAutomovel;
+	int numerocliente;
+	char nomecliente[100];
+	char nif[10]; // por culpa do \0
+	float valor;
+	TipoCliente tipo;
+	char dataregisto[11];
+}TCAutomovel;
 
-
-void InsereCliente(TCAutomovel cliente[], int* contador);
-void GravarClientes(TCAutomovel clientes[], int contador, const char* nomeFicheiro);
-int LerClientes(TCAutomovel cliente[], const char* nomeFicheiro);
-void ListarClientes(TCAutomovel consultas[], int contador);
-void OrdenarClientesNome(TCAutomovel consultas[], int contador);
-int LerClientes(const char* nome_ficheiro, TCAutomovel** vetor);
+void InserirCliente(TCAutomovel cliente[], int* contador);
+void ListarCliente(TCAutomovel clientes[], int *contador);
+int LerClientes(TCAutomovel clientes[], const char* nomeFicheiro);
+void ListarClientePorTipo(TCAutomovel cliente[], int *contador);
+void GravarClientes(TCAutomovel clientes[], const char* nomeFicheiro);
 void OrdenarClientes(TCAutomovel clientes[], int contador, int ordem);
-void RemoverClientePorNumero(TCAutomovel clientes[], int* contador, int numerocliente);
-
+void RemoverClientePorNumero(TCAutomovel cliente[], int* contador, int numerocliente);
+void CalcularValorTotal(TCAutomovel cliente[], int contador);
